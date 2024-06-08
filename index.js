95ef808e9fd750618ea2996e7fc70a45b98c578c
omkar mhaske <135412726+omkarmaske160@users.noreply.github.com>
2024-06-08 12:45:15 +0530

    update

--- index.js
+++ index.js
@@ -7,33 +7,33 @@ require('dotenv').config({ path: './.env' })
 
 // Db
 mongoose.connect(process.env.MONGO_URL)
- const app = express()
+const app = express()
 // MiddleWares
-app.use(express.json()) 
+app.use(express.json())
 // ^ to access the data of body
 app.use(cookieParser())
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: "https://url-shotner-seven.vercel.app/",
+    credentials: true
 }))
 
 // routes
 app.use('/api/v1/auth', require('./routes/authRoute'))
-app.use('/api/v1/user', userProtected , require('./routes/userRoutes'))
+app.use('/api/v1/user', userProtected, require('./routes/userRoutes'))
 app.use('/api/v1/url', require('./routes/urlRoute'))
-app.use('/api/v1/admin', adminProtected , require('./routes/adminRoute'))
+app.use('/api/v1/admin', adminProtected, require('./routes/adminRoute'))
 
 
 // 404 
 app.use("*", (req, res) => {
-    res.status(404).json({message : "resource not found"})
+    res.status(404).json({ message: "resource not found" })
 })
 
 // Error Handler 
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
     console.log(err)
-    res.status(500).json({ message : err.message || "something went Wrong"})
+    res.status(500).json({ message: err.message || "something went Wrong" })
 })
 
 // server
