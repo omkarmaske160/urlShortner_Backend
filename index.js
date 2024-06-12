fdf717eede222cc297e6947c3d8334aa66c4ad87
omkar mhaske <135412726+omkarmaske160@users.noreply.github.com>
2024-06-12 10:08:04 +0530

    upadate

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(express.json())
 // ^ to access the data of body
 app.use(cookieParser())
 
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
 app.use(cors({
     origin: "https://url-shortner-2dr5.onrender.com",
     credentials: true
